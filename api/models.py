85e0c45121cb6ce282aab2fbe3a61f91c3ade786
Hao Liu <liuhao1990@gmail.com>
2015-08-28 11:38:01 -0400

    add image

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid, os
 
 class Category(models.Model):
 	name = models.CharField(max_length=30)
@@ -7,10 +8,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+def author_avatar_name(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('media/author_avatar', filename)
+
 class Vendor(models.Model):
 	name = models.CharField(max_length=30)
     url = models.URLField()
-
+    avatar = models.ImageField(upload_to=author_avatar_name)
     def __str__(self):
         return self.name
 
