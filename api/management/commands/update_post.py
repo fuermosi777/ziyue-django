97f349e39b4f639bd4ab07d7eaf97560cc6592b8
Hao Liu <liuhao1990@gmail.com>
2015-08-30 13:38:02 -0400

    d

--- api/management/commands/update_post.py
+++ api/management/commands/update_post.py
@@ -3,13 +3,25 @@ from django.core.management.base import BaseCommand, CommandError
 from api.models import *
 from datetime import datetime, timedelta
 from processor import storer
+from multiprocessing import Pool
+from django.db import connection
+
+def update_vendor_post(vendor):
+    connection.close()
+    spider = getattr(__import__('processor.spider', fromlist=[str(vendor.slug)]), str(vendor.slug))
+    post_list = spider.crawl()
+    for p in post_list:
+        storer.store(vendor, p)
 
 class Command(BaseCommand):
     help = ""
     def handle(self, *args, **options):
         vendors = Vendor.objects.all()
+        vendor_list = []
         for v in vendors:
-            spider = getattr(__import__('processor.spider', fromlist=[str(v.slug)]), str(v.slug))
-            post_list = spider.crawl()
-            for p in post_list:
-                storer.store(v, p)
+            vendor_list.append(v)
+
+        pool = Pool(6)
+        pool.map(update_vendor_post, vendor_list)
+        pool.close()
+        pool.join()
