a9edf833cd08c33023bc697c0298c770d7a9d206
Hao Liu <liuhao1990@gmail.com>
2015-09-02 23:13:27 -0400

    fix cnbeta cr

--- processor/spider/cnbeta.py
+++ processor/spider/cnbeta.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.list_rss_crawl(url='http://rss.cnbeta.com/rss', title_select='h2.news_title', body_select='section.article_content', remove_tags=[])
+    return basic.list_rss_crawl(url='http://rss.cnbeta.com/rss', title_select='h2#news_title', body_select='section.article_content', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
