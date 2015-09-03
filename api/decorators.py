b028c2873b550024427e8856ab9d3308963c0354
Hao Liu <liuhao1990@gmail.com>
2015-09-02 23:11:58 -0400

    d

--- api/decorators.py
+++ api/decorators.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 def domain_verify(func=None):
     def _decorated(view_func):
         def _view(request, *args, **kwargs):
-            if (request.META['HTTP_HOST'] not in ['ziyue.io', '127.0.0.1:8000', 'localhost:8000']):
+            if (request.META['HTTP_HOST'] not in ['ziyue.io', 'www.ziyue.io', '127.0.0.1:8000', 'localhost:8000']):
                 return HttpResponse(status=503)
             else:
                 return view_func(request, *args, **kwargs)
