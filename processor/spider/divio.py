5f65bc63566ab266816456b439a280cf8093de1b
Hao Liu <liuhao1990@gmail.com>
2015-09-05 20:28:23 -0400

    add

--- processor/spider/divio.py
+++ processor/spider/divio.py
@@ -5,7 +5,7 @@ def crawl():
         url='http://div.io/pro/index', 
         list_select='.hot-topics li span.topic-title a.title', 
         list_url_pre='', 
-        title_select='.topic-main .topic-firstfloor-info h1', 
+        title_select='tit', 
         body_select='.topic-firstfloor-detail'
     )
 
