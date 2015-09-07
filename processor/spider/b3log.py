74b680402c52d9469f0308794f8745d25a0027fb
Hao Liu <liuhao1990@gmail.com>
2015-09-07 09:48:11 -0400

    fix v

--- processor/spider/b3log.py
+++ processor/spider/b3log.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.std_rss_crawl('http://88250.b3log.org/blog-articles-rss.do')
+    return basic.list_rss_crawl(url='http://88250.b3log.org/blog-articles-rss.do', title_select='title', body_select='section.article-body', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
