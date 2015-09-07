5e7e6747bc06764f445b1433eda1fc4a12cdc9d8
Hao Liu <liuhao1990@gmail.com>
2015-09-07 12:53:40 -0400

    fix oschina sources

--- processor/spider/oschina_blog.py
+++ processor/spider/oschina_blog.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.list_rss_crawl(url='http://www.oschina.net/blog/rss', title_select='.BlogTitle h1', body_select='.BlogContent', remove_tags=[])
+    return basic.hard_crawl(url='http://www.oschina.net/blog/rss', list_select='.BlogList li h3 a', title_select='.BlogTitle h1', body_select='.BlogContent', list_url_pre='', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
--- processor/spider/oschina_news.py
+++ processor/spider/oschina_news.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.list_rss_crawl(url='http://www.oschina.net/news/rss', title_select='.BlogTitle h1', body_select='.BlogContent', remove_tags=[])
+    return basic.hard_crawl(url='https://www.oschina.net/news/list?show=industry', list_select='ul.List li h2 a', title_select='h1.OSCTitle', body_select='.NewsContent', list_url_pre='https://www.oschina.net', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
