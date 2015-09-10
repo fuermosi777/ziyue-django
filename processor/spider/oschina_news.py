22e7408cdacc0e303c5137f0efa03665f13d5a26
Hao Liu <liuhao1990@gmail.com>
2015-09-10 13:08:28 -0400

    fix oschina news

--- processor/spider/oschina_news.py
+++ processor/spider/oschina_news.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.hard_crawl(url='https://www.oschina.net/news/list?show=industry', list_select='ul.List li h2 a', title_select='h1.OSCTitle', body_select='.NewsContent', list_url_pre='https://www.oschina.net', remove_tags=[])
+    return basic.list_rss_crawl(url='http://www.oschina.net/news/rss?show=industry', title_select='h1.OSCTitle', body_select='.NewsContent', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
