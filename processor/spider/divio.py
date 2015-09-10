0ff3eb5718da3df9f21fe0caf01f7ea80fc76ee5
Hao Liu <liuhao1990@gmail.com>
2015-09-10 13:01:16 -0400

    fix oschina

--- processor/spider/divio.py
+++ processor/spider/divio.py
@@ -5,7 +5,7 @@ def crawl():
         url='http://div.io/pro/index', 
         list_select='.hot-topics li span.topic-title a.title', 
         list_url_pre='', 
-        title_select='tit', 
+        title_select='title', 
         body_select='.topic-firstfloor-detail'
     )
 
--- processor/spider/oschina_blog.py
+++ processor/spider/oschina_blog.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.hard_crawl(url='http://www.oschina.net/blog/rss', list_select='.BlogList li h3 a', title_select='.BlogTitle h1', body_select='.BlogContent', list_url_pre='', remove_tags=[])
+    return basic.hard_crawl(url='http://www.oschina.net/blog/more?p=1', list_select='.BlogList li h3 a', title_select='title', body_select='.BlogContent', list_url_pre='', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
