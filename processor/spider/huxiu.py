ab0d50b5aa8557593bfdf21c488ed0c2ccaa7a40
Hao Liu <liuhao1990@gmail.com>
2015-09-16 18:51:37 -0400

    fix huxiu

--- processor/spider/huxiu.py
+++ processor/spider/huxiu.py
@@ -1,27 +1,7 @@
 import basic
-from datetime import datetime
 
 def crawl():
-    res = []
-    list = get_list()
-    for l in list:
-        res.append(get_post(l))
-    return res
-
-def get_list():
-    res = []
-    list = basic.load_json('http://m.api.huxiu.com/portal/1/1?client_ver=6&push_type=iOSRel')
-    for l in list['content']:
-        res.append('http://m.api.huxiu.com/article/%s?client_ver=6&push_type=iOSRel'%l['aid'])
-    return res
-
-def get_post(url):
-    post = basic.load_json(url)['content']
-    return {
-        'title': post['title'],
-        'body': post['content'],
-        'source': post['url'],
-    }
+    return basic.list_rss_crawl(url='http://www.huxiu.com/rss/0.xml', title_select='h1.t-h1', body_select='.article_content', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
