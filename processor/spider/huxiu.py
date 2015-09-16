51f1cb5bdb4c70ac3cb5d0f549257273f3772d2c
Hao Liu <liuhao1990@gmail.com>
2015-09-16 18:52:28 -0400

    fix huxiu

--- processor/spider/huxiu.py
+++ processor/spider/huxiu.py
@@ -1,7 +1,7 @@
 import basic
 
 def crawl():
-    return basic.list_rss_crawl(url='http://www.huxiu.com/rss/0.xml', title_select='h1.t-h1', body_select='.article_content', remove_tags=[])
+    return basic.list_rss_crawl(url='http://www.huxiu.com/rss/0.xml', title_select='h1.t-h1', body_select='.article-content-wrap', remove_tags=[])
 
 if __name__ == "__main__":
     crawl()
