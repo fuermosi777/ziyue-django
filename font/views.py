3bcb7a6acce66bf7595e75ff508a09c1a991be3c
Hao Liu <liuhao1990@gmail.com>
2015-09-17 23:28:37 -0400

    update

--- font/views.py
+++ font/views.py
@@ -18,5 +18,5 @@ def main(request):
     body = body.encode('utf8') # encode
     font_id = uuid.uuid4()
     subprocess.call(['java', '-jar', 'font/dist/tools/sfnttool/sfnttool.jar', '-e', '-s', '%s'%body, 'font/fonts/PingFangRegular.ttf', 'font/serve/%s.eot'%font_id])
-    content = '@font-face {font-family: "Ping-Fang"; font-style: normal; font-weight: 400; src: local("PingFang"), url(/fonts/%s.woff) format("woff"); }'%font_id
+    content = '@font-face {font-family: "Ping-Fang"; font-style: normal; font-weight: 400; src: local("PingFang"), url(/fonts/%s.eot) format("eot"); }'%font_id
     return HttpResponse(content, content_type='text/css')
