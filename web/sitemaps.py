c594ee5fa9bef394369832b3997a789b02f54798
Hao Liu <liuhao1990@gmail.com>
2015-09-20 12:41:27 -0400

    f ix

--- web/sitemaps.py
+++ web/sitemaps.py
@@ -1,5 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from api.models import *
+from api.encrypter import encode
 
 class WebSitemap(Sitemap):
     changefreq = "weekly"
@@ -7,3 +8,6 @@ class WebSitemap(Sitemap):
 
     def items(self):
         return Post.objects.order_by('-datetime')[:500]
+
+    def get_absolute_url(self):
+        return '/post/%s'%encode(self.id)
